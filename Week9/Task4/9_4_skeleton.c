de06f0dce87ff2a480a11e28054fd52ec0918544
sihadan <def6488@gmail.com>
2017-11-01 21:28:12 +0900

    add skeleton code 9-4

---
+++ Week9/Task4/9_4_skeleton.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+// Implement below functions
+int my_union(int setA, int setB){
+}
+int my_intersection(int setA, int setB){
+}
+int my_add(int setA, int x){
+}
+int my_delete(int setA, int x){
+}
+int my_setminus(int setA, int setB){
+}
+void my_print(int setA){
+}
+
+int main(){
+	int setA = 0, setB = 0;
+	while(1){
+		char op[30], opA[10], dummy[10];
+		scanf("%s",op);
+
+		if( strcmp(op,"quit") == 0 ) break;
+		
+		if( strcmp(op,"print") == 0 ){
+			scanf("%s",opA);
+			if( strcmp(opA,"A") == 0 ) my_print(setA);
+			if( strcmp(opA,"B") == 0 ) my_print(setB);
+		}
+		
+		if( strcmp(op,"add") == 0 ){
+			int x; scanf("%d",&x);
+			scanf("%s%s",dummy,opA);
+			if( strcmp(opA,"A") == 0 ) setA = my_add(setA,x);
+			if( strcmp(opA,"B") == 0 ) setB = my_add(setB,x);
+		}
+		
+		if( strcmp(op,"delete") == 0 ){
+			int x; scanf("%d",&x);
+			scanf("%s%s",dummy,opA);
+			if( strcmp(opA,"A") == 0 ) setA = my_delete(setA,x);
+			if( strcmp(opA,"B") == 0 ) setB = my_delete(setB,x);
+		}
+		
+		if( strcmp(op,"union") == 0 ) setA = my_union(setA,setB);
+		
+		if( strcmp(op,"intersection") == 0 ) setA = my_intersection(setA,setB);
+
+		if( strcmp(op,"setminus") == 0 ) setA = my_setminus(setA,setB);
+		
+	}
+}

bde200838f8d47f672310edfe6e2cde0d6b6ba6a
Sujae Lee <sujaelee7940@gmail.com>
2017-11-23 13:28:25 +0900

    Create task1_skeleton.c

---
+++ week12/task1_skeleton.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+
+}
+ 
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+
+}
+
+//comment below when submit
+/*
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int* arr = (int *)malloc(sizeof(int) * n);
+    for(int i = 0 ; i < n ; i++){
+    scanf("%d", &arr[i]);
+    }
+
+    mergeSort(arr, 0, n-1);
+
+    for(int i = 0 ; i < n ; i++){
+    printf("%d ", arr[i]);
+    }
+ 
+    return 0;
+}
+*/
